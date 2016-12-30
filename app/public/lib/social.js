7177e50f7ff9f117f6fb37b7f7f369202481fc37
Brendan Chan <brendangkchan@gmail.com>
2016-12-30 15:57:09 -0800

    fix recompile

---
+++ app/public/lib/social.js
@@ -0,0 +1,8 @@
+export default [{
+	image: 'https://d2jk9tf9979qo8.cloudfront.net/social_fb.svg',
+	url: 'https://facebook.com/bgkchan'
+},
+{
+	image: 'https://d2jk9tf9979qo8.cloudfront.net/social_ig.svg',
+	url: 'https://instagram.com/bgkchan'
+}]
