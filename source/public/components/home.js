b8bb39a4453bc9f056e65268642a69ae750bd0d3
Brendan Chan <brendangkchan@gmail.com>
2017-01-05 23:09:49 -0800

    home start here link to work

--- source/public/components/home.js
+++ source/public/components/home.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Menu from './menu'
+import Button from 'react-bootstrap/lib/Button'
+import { Link } from 'react-router'
 
 const Home = () => {
   return (
   	<div>
 			<div id='home_wrapper'>
 				<div className='background' />
+				<div className='home_work-button-container'>
+					<Link to='/work'>
+	  				<h2 className='home_work-button'>Start here</h2>
+					</Link>
+				</div>
 			</div>
 		</div>
 	)
