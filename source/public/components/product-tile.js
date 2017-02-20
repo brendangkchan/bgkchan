b44279aed5f6385ca505fe2dc6222ce760416c8d
Brendan Chan <brendangkchan@gmail.com>
2017-02-19 18:09:49 -0800

    product-tile: use lodash get for product props

--- source/public/components/product-tile.js
+++ source/public/components/product-tile.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import get from 'lodash.get'
 
 const ProductTile = ({ product, setProduct }) => {
+	const title = get(product, 'title', '')
+	const image = get(product, 'images[0].src', '')
+	const price = Math.round(get(product, 'variants[0].price', 0))
+
 	return (
 		<div className='product-tile_wrapper' onClick={setProduct.bind(null, product)}>
 			<div className='product-tile_image-wrapper'>
-				<img className='product-tile_image' src={product.images[0].src} alt={product.title} />	
+				<img className='product-tile_image' src={image} alt={title} />	
 			</div>
 			<div className='product-tile_text'>
 				<div className='product-tile_title'>{ product.title }</div>
-				<div className='product-tile_price'>{ `$${Math.round(product.variants[0].price)}` }</div>
+				<div className='product-tile_price'>{ `$${price}` }</div>
 			</div>
 		</div>
 	)
